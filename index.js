167f09df5dc5c96f6fc380a5a84055b390a20ce2
Cole Simpson <cole22simpson@gmail.com>
2022-04-15 22:45:42 -0700

    created manager object

--- index.js
+++ index.js
@@ -4,69 +4,66 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
-const promptUser = employeeData => {
+function createManager() {
+    this.teamMembers = [];
+
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'name',
-                message: "What is this employee's name?"
+                message: "What is this manager's name?"
             },
             {
                 type: 'input',
                 name: 'id',
-                message: 'Enter a unique ID number for this employee.'
+                message: "What is this manager's unique ID number?"
             },
             {
                 type: 'input',
                 name: 'email',
-                message: "What is this employee's email?"
+                message: "What is this manager's email?"
             },
             {
-                type: 'list',
-                name: 'role',
-                message: "What is this employee's role?",
-                choices: ['Manager', 'Engineer', 'Intern']
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is this manager's office number?"
+            },
+            {
+                type: 'confirm',
+                name: 'confirmTeam',
+                message: 'Would you like to add any more team members?',
+                default: false
             }
         ])
-        .then(({ role }) => {
-            if (role === 'Manager') {
-                inquirer
-                    .prompt({
-                        type: 'input',
-                        name: 'officeNumber',
-                        message: "What is this manager's office number?"
-                    })
-                    .then(({ officeNumber }) => {
-                        const manager = new Manager(officeNumber);
-                        console.log(manager);
-                    });
-            }
-            else if (role === 'Engineer') {
-                inquirer
-                    .prompt({
-                        type: 'input',
-                        name: 'github',
-                        message: "What is this engineer's GitHub username?"
-                    })
-                    .then(({ github }) => {
-                        const engineer = new Engineer(github);
-                        console.log(engineer);
-                    })
-            }
-            else {
-                inquirer
-                    .prompt({
-                        type: 'input',
-                        name: 'school',
-                        message: "What school does this intern attend?"
-                    })
-                    .then(({ school }) => {
-                        const intern = new Intern(school);
-                        console.log(intern);
-                    })
-            }
-        });
+        .then(( answers ) => {
+            console.log(answers);
+            this.manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            console.log(this.manager);
+            this.teamMembers.push(this.manager[0]);
+        })
 };
 
-promptUser();
+createManager();
+
+// create a empty array
+//function createManager()
+// last question would be would you like to add team members
+// .then()
+// new Manager(name, phone, email.....)
+// push the manager to the empty array
+
+
+//function createTeam()
+// prompt would ask what team member would 
+// if (intern)
+// call a function known as create intern
+
+//function create intern()
+//propmt would ask relevant questions
+// last question would be would you like to add team members
+// new Intern(name, phone, email.....)
+// push the intern to the empty array
+// if (would you like to add team members === true)
+// call a function known as create team
+// else create HTML
