e8506b48ae90b9d091d41b35c14714758aa28444
Cole Simpson <cole22simpson@gmail.com>
2022-04-15 23:52:12 -0700

    successfully creates all objects and will allow endless user input

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
+var teamMembers = [];
+
 function createManager() {
-    this.teamMembers = [];
 
     inquirer
         .prompt([
@@ -37,10 +38,124 @@ function createManager() {
             }
         ])
         .then(( answers ) => {
-            console.log(answers);
             this.manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-            console.log(this.manager);
-            this.teamMembers.push(this.manager[0]);
+            teamMembers.push(this.manager);
+            console.log(teamMembers);
+
+            if (answers.confirmTeam) {
+                createTeam();
+            }
+            else {
+                console.log("Your team profile has been created!");
+                // Create file here later
+            }
+        })
+};
+
+function createTeam() {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'role',
+            message: 'Is your team member an engineer or an intern?',
+            choices: ['Engineer', 'Intern']
+        })
+        .then(({ role }) => {
+            if (role === 'Intern') {
+                createIntern();
+            }
+            else {
+                createEngineer();
+            }
+        })
+};
+
+function createIntern() {
+    inquirer    
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is this intern's name?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is this intern's unique ID Number?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is this intern's email?"
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Where does this intern attend school?'
+            },
+            {
+                type: 'confirm',
+                name: 'confirmTeam',
+                message: 'Do you want to add another team member?'
+            }
+        ])
+        .then(( answers ) => {
+            this.intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            teamMembers.push(this.intern);
+            console.log(teamMembers);
+
+            if (answers.confirmTeam) {
+                createTeam();
+            }
+            else {
+                console.log("Your team profile has been created!");
+                // Create file here later
+            }
+        })
+};
+
+function createEngineer() {
+    inquirer    
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is this engineer's name?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is this engineer's unique ID Number?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is this engineer's email?"
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is this engineer's GitHub username?"
+            },
+            {
+                type: 'confirm',
+                name: 'confirmTeam',
+                message: 'Do you want to add another team member?',
+                default: false
+            }
+        ])
+        .then(( answers ) => {
+            this.engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            teamMembers.push(this.engineer);
+            console.log(teamMembers);
+
+            if (answers.confirmTeam) {
+                createTeam();
+            }
+            else {
+                console.log("Your team profile has been created!");
+                // Create file here later
+            }
         })
 };
 
